237fd7dac26b3926297c698081ac79281d694a03
pixelthieves <pixelthievesgames@gmail.com>
2016-01-05 23:34:22 +0100

    Setting up events.

--- ElementTD/Assets/Scripts/TowerMap.cs
+++ ElementTD/Assets/Scripts/TowerMap.cs
@@ -1,17 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using Assets.Shared.Scripts;
+using TouchScript.Gestures;
+using TouchScript.Hit;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
+    [RequireComponent(typeof (Collider), typeof (TapGesture))]
     public class TowerMap : MonoBehaviour
     {
         public int Width;
         public int Height;
 
-        Tower[,] map;
+        private Tower[,] map;
+        private Bounds bounds;
 
         private void Start()
         {
-             map = new Tower[Width, Height];
+            bounds = GetComponent<Collider>().bounds;
+            map = new Tower[Width, Height];
+        }
+
+        private void OnEnable()
+        {
+            GetComponent<TapGesture>().Tapped += OnTapped;
+        }
+
+        private void OnDisable()
+        {
+            GetComponent<TapGesture>().Tapped -= OnTapped;
+        }
+
+        private void OnTapped(object sender, EventArgs e)
+        {
+            var gesture = sender as TapGesture;
+            TouchHit hit;
+            gesture.GetTargetHitResult(out hit);
+
+            var normalizedPoint = (hit.Point - bounds.min).Divide(bounds.size);
+            OnTileTapped((int) (normalizedPoint.x*Width), (int) (normalizedPoint.z*Height));
+        }
+
+        private void OnTileTapped(int x, int y)
+        {
+            Debug.Log("Tile Tapped on [" + x + "," + y + "]");
         }
     }
 }
