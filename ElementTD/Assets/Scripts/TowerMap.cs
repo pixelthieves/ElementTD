588aed2fb33b78f7c2a50f65005d0305f105d367
pixelthieves <pixelthievesgames@gmail.com>
2016-01-05 22:03:46 +0100

    Base implementation of TowerMap and Tower scripts.

---
+++ ElementTD/Assets/Scripts/TowerMap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TowerMap : MonoBehaviour
+    {
+        public int Width;
+        public int Height;
+
+        Tower[,] map;
+
+        private void Start()
+        {
+             map = new Tower[Width, Height];
+        }
+    }
+}
