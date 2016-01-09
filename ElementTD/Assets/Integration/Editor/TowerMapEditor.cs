6fe97fdfdd0db189375bbb82ba33d82a58be7495
pixelthieves <pixelthievesgames@gmail.com>
2016-01-09 18:10:52 +0100

    Reorganizing project.

---
+++ ElementTD/Assets/Integration/Editor/TowerMapEditor.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    [CustomEditor(typeof (TowerMap))]
+    public class TowerMapEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var towerMap = (TowerMap) target;
+            var tileSize = new Vector2(50, 50);
+            GUILayout.Space(8);
+            var rect = EditorGUILayout.GetControlRect();
+            for (var i = 0; i < towerMap.Map.Width; i++)
+            {
+                for (var j = 0; j < towerMap.Map.Height; j++)
+                {
+                    var position = new Rect(rect.x + i*tileSize.x, rect.y + j*tileSize.y, tileSize.x, tileSize.y);
+                    towerMap.Map[i, j] = !GUI.Toggle(position, !towerMap.Map[i, j], "", "Button");
+                }
+            }
+            GUILayout.Space(towerMap.Map.Height*tileSize.y);
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(towerMap);
+            }
+        }
+    }
+}
