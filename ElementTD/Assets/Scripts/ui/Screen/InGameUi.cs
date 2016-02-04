b88d730991fa95455d79becd9b60f732cfa7c98e
pixelthieves <pixelthievesgames@gmail.com>
2016-02-04 22:22:27 +0100

    Adding game ui mockup.

---
+++ ElementTD/Assets/Scripts/ui/Screen/InGameUi.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class InGameUi : MonoBehaviour
+    {
+        public Button PauseButton;
+        public Button SlowDownButton;
+        public Text CurrentSpeedText;
+        public Button SpeedUpButton;
+        public Text LivesText;
+        public Text GoldText;
+        public Text WaveText;
+        public Text InterestText;
+        public Image NextWaveImage;
+        public Text NextWaveText;
+        public Button NextWaveButton;
+        public RectTransform TowerGrid;
+    }
+}
