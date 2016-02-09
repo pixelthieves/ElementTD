7b9c2e4f2e8168e8e7daae5bcef540e3c7def6ad
pixelthieves <pixelthievesgames@gmail.com>
2016-02-09 22:07:01 +0100

    Adding speed multiplier.

---
+++ ElementTD/Assets/Scripts/ui/GameSpeedMultiplier.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class GameSpeedMultiplier : MonoBehaviour
+    {
+        public float Min;
+        public float Max;
+        public float Step;
+
+        public Text Output;
+
+        private void Start()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            SetCurrentSpeed(1);
+        }
+
+        public void SpeedUp()
+        {
+            SetCurrentSpeed(Time.timeScale * Step);
+        }
+
+        public void SpeedDown()
+        {
+            SetCurrentSpeed(Time.timeScale / Step);
+        }
+
+        private void SetCurrentSpeed(float value)
+        {
+            Time.timeScale = Mathf.Clamp(value, Min, Max);
+            Output.text = Time.timeScale + "x";
+        }
+    }
+}
